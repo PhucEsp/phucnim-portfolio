2cd9f2f2085ba490b6dbec6f17fe9fe2c5b6e5bf
Nim Tri Phuc <49029303+PhucEsp@users.noreply.github.com>
2023-05-10 14:45:04 +0700

    feat: add prettier (#1)
    Co-authored-by: prd-phuc-nim <phuc.nim@paradox.ai>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,15 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es6: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,12 @@
-import Sammy from '../public/image/sammy.jpg'
-import "../assets/css/welcome.css"
+import Sammy from '../public/image/sammy.jpg';
+import '../assets/css/welcome.css';
 
 export default function Welcome() {
-    return(
-        <div className="wrapper">
-            <h1>Welcome To My App</h1>
-            <p>This is Phuc Nim portfolio page!</p>
-            <img src={Sammy} alt="Sammy Image" width={200} height={200} />
-        </div>
-    )
+  return (
+    <div className="wrapper">
+      <h1>Welcome To My App</h1>
+      <p>This is Phuc Nim portfolio page!</p>
+      <img src={Sammy} alt="Sammy Image" width={200} height={200} />
+    </div>
+  );
 }
