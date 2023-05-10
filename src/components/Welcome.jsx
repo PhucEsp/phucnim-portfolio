28b70bd892e59c4152a0b5760740fb8ad89693c5
Nim Tri Phuc <49029303+PhucEsp@users.noreply.github.com>
2023-05-10 15:29:54 +0700

    chore: check deployment (#2)
    Co-authored-by: prd-phuc-nim <phuc.nim@paradox.ai>

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,8 +4,8 @@ import '../assets/css/welcome.css';
 export default function Welcome() {
   return (
     <div className="wrapper">
-      <h1>Welcome To My App</h1>
-      <p>This is Phuc Nim portfolio page!</p>
+      <h1>Welcome To My portfolio</h1>
+      <p>My name is Phuc Nim</p>
       <img src={Sammy} alt="Sammy Image" width={200} height={200} />
     </div>
   );
