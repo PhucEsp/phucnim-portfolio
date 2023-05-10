aaaf5b7dcb5df3673e7cca357f6d67970ee7f972
prd-phuc-nim <phuc.nim@paradox.ai>
2023-05-10 14:43:29 +0700

    feat: add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Welcome from "./components/Welcome";
+import Welcome from './components/Welcome';
 export default function App() {
   return (
-  <div>
-    <Welcome/>
-  </div>
+    <div>
+      <Welcome />
+    </div>
   );
 }
