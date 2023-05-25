6e82199f404336e657b6cc3abf2e02400633a45f
Nim Tri Phuc <49029303+PhucEsp@users.noreply.github.com>
2023-05-25 22:03:05 +0700

    Feat/update project (#3)
    * feat: build layout and apply threejs
    
    * chore: remove feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import Welcome from './components/Welcome';
-export default function App() {
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
   return (
-    <div>
-      <Welcome />
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <Feedbacks /> */}
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
+
+export default App;
